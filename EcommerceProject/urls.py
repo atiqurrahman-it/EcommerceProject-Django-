08f7c1b2054072799024204275dba6bf7a652092
atiqur <atiqurrahman.cse2552@gmail.com>
2022-10-01 17:09:12 +0600

    envfile

--- EcommerceProject/urls.py
+++ EcommerceProject/urls.py
@@ -3,6 +3,8 @@
 from django.contrib import admin
 from django.urls import path,include
 
+from django.conf import settings
+from django.conf.urls.static import static
 urlpatterns = [
     path('admin/', admin.site.urls),
      path('', include('Ecommerce_app.urls')),
@@ -10,3 +12,10 @@ urlpatterns = [
     path('OrderApp/', include('OrderApp.urls')),
     path('user_app/', include('user_app.urls')),
 ]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.STATIC_URL,
+                          document_root=settings.STATIC_ROOT)
+    urlpatterns += static(settings.MEDIA_URL,
+                          document_root=settings.MEDIA_ROOT)
+
